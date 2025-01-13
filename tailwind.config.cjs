8cd3da6a8963292f20ef69ecfd74f0f95c0dcd7a
shamshuddinmgm <shamshuddinmgm@gmail.com>
2025-01-14 00:18:58 +0530

    theme keeda bhai fix

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,7 +6,7 @@ module.exports = {
 	},
 	plugins: [require("@tailwindcss/typography"),require("daisyui")],
 	daisyui: {
-		themes: ["dark","light","retro","forest","aqua","black","luxury","dracula","night","coffee"], // true: all themes | false: only light + dark | array: specific themes like this ["light", "dark", "cupcake"]
+		themes: ["dark", "light", "retro", "forest", "aqua", "black", "luxury", "dracula", "night", "coffee"], // true: all themes | false: only light + dark | array: specific themes like this ["light", "dark", "cupcake"]
 		darkTheme: "forest", // name of one of the included themes for dark mode
 		logs: false, // Shows info about daisyUI version and used config in the console when building your CSS
 	  }
